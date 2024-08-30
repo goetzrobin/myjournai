11335874ac73b209b2b29fe802a5b04629279d92
robingotz <tug29225@temple.edu>
2024-08-30 22:02:39 +0200

    feat: match vercel output api

--- apps/nitro-app/nitro.config.ts
+++ apps/nitro-app/nitro.config.ts
@@ -1,14 +1,17 @@
 //https://nitro.unjs.io/config
 import { join } from 'path';
 import { workspaceRoot } from '@nx/devkit';
+import { resolve } from 'node:path';
+import { normalizePath } from 'vite';
 
 export default defineNitroConfig({
   rootDir: '../..',
   srcDir: 'apps/nitro-app/server',
   output: {
-    dir: join(workspaceRoot, 'dist', 'apps', 'nitro-app', '.output'),
-    serverDir: join(workspaceRoot, 'dist', 'apps', 'nitro-app', '.output/server'),
-    publicDir: join(workspaceRoot, 'dist', 'apps', 'nitro-app', '.output/public')
+    dir: normalizePath(resolve(workspaceRoot, '.vercel', 'output')),
+    publicDir: normalizePath(
+      resolve(workspaceRoot, '.vercel', 'output/static')
+    )
   },
   publicAssets: [{
     baseURL: '',
@@ -19,5 +22,5 @@ export default defineNitroConfig({
     anthropicApiKey: process.env.NITRO_ANTHROPIC_API_KEY,
     groqApiKey: process.env.NITRO_GROQ_API_KEY
   },
-  modules: ['./modules/tsconfig-paths-to-aliases.ts'],
+  modules: ['./modules/tsconfig-paths-to-aliases.ts']
 });
