fcec67c594f7855fe4f941d0ef84875b6b5cb327
robingotz <tug29225@temple.edu>
2024-09-04 14:29:51 +0200

    feat: change graduation year minimum

--- libs/onboarding/shared/src/lib/user-onboarding-data.ts
+++ libs/onboarding/shared/src/lib/user-onboarding-data.ts
@@ -6,7 +6,7 @@ export const UserOnboardingDataSchema = z.object(
     genderIdentity: z.string(),
     ethnicity: z.string(),
     ncaaDivision: z.string(),
-    graduationYear: z.number().min(2019).max(2025)
+    graduationYear: z.number().min(2010).max(2025)
   });
 
 export type UserOnboardingData = z.infer<typeof UserOnboardingDataSchema>
