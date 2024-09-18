078d4cc52fb9a3ef9e2f5abd3632fd78b8e584f3
robingotz <tug29225@temple.edu>
2024-09-18 13:52:19 +0200

    fix: invalidate user after completing onboarding

--- libs/user/client/src/lib/queries/complete-onboarding.mutation.ts
+++ libs/user/client/src/lib/queries/complete-onboarding.mutation.ts
@@ -1,11 +1,14 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useAxios } from '~myjournai/http-client';
+import { userQKF } from './query-key.factory';
 
 export const useUserCompleteOnboardingMutation = ({ userId }: { userId?: string }, onSuccess?: () => void) => {
   const axios = useAxios();
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: () => axios.post(`/api/users/${userId}/onboarding/complete`),
     onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: userQKF.detail(userId) });
       onSuccess?.();
     }
   });
