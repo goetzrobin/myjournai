7ca05e9c036f5f7901ff442c7b8a09af08c4fae4
robingotz <tug29225@temple.edu>
2024-10-25 16:29:36 -0400

    fix: typos

--- apps/study-prototype/src/routes/_app/onboarding/survey-intro.tsx
+++ apps/study-prototype/src/routes/_app/onboarding/survey-intro.tsx
@@ -8,8 +8,8 @@ export const Route = createFileRoute('/_app/onboarding/survey-intro')({
       <div className="place-self-center w-full">
         <h1 className="text-2xl text-center">Let's start with a quick survey</h1>
         <p className="mt-4 text-muted-foreground text-center">As part of our research with Temple we are collecting
-          anonymized demographic amd career readiness data. The following survey will be shared with our research
-          partners Dr.
+          anonymized demographic and career readiness data. The following survey will be shared with our research
+          partners Drs.
           Elizabeth Taylor and Jessica Brougham.</p>
       </div>
       <Link className="absolute left-0 right-0 bottom-4" to="/onboarding/study/user">
