20fb4b0660814449de038893b62bd79b5f3782f1
robingotz <tug29225@temple.edu>
2025-03-12 16:17:17 +0100

    feat: increase timeout

--- apps/nitro-app/nitro.config.ts
+++ apps/nitro-app/nitro.config.ts
@@ -26,7 +26,7 @@ export default defineNitroConfig({
   modules: ['./modules/tsconfig-paths-to-aliases.ts'],
   vercel: {
     functions: {
-      maxDuration: 60
+      maxDuration: 120
     }
   }
 });
